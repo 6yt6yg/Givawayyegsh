d41c5c0d63e91ce41fabb6499471db9221d10117
Glitch (hello-express) <none>
2020-09-26 18:34:22 +0000

    🐫🎆 Checkpoint ./server.js:11517431/12021

--- server.js
+++ server.js
@@ -382,7 +382,39 @@ client.on("message", async message => {
     } else return undefined;
   }
 })
+const developers = ["505442667652448277","id"]
+client.on('message', message => {
+    var argresult = message.content.split( ).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
 
+  if (message.content.startsWith(adminprefix + 'setg')) {
+    client.user.setGame(argresult);
+      message.channel.send(**✅   ${argresult}**)
+  } else 
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();
+  } else
+  if (message.content.startsWith(adminprefix + 'setw')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(**✅   ${argresult}**)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setl')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(**✅   ${argresult}**)
+  } else 
+  if (message.content.startsWith(adminprefix + 'sets')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/dream%22);
+      message.channel.send(**✅**)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(Changing The Name To ..**${argresult}** )
+} else
+if (message.content.startsWith(adminprefix + 'setava')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(Changing The Avatar To :**${argresult}** );
+}
+});
 
 client.login(process.env.MG_TOKEN)
 
